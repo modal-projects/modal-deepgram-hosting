de3167f157849f72f4835a640a5702b35bcacad4
shababo <ben.shababo@modal.com>
2026-01-28 17:41:23 -0800

    remove download_files file

--- download_models.py
+++
@@ -1,9 +0,0 @@
-import modal
-
-app = modal.App("download-models")
-models_vol = modal.Volume.from_name("deepgram-models", create_if_missing=True)
-
-import urllib.error
-import urllib.request
-from pathlib import Path
-
